5fe6c93800d208c0a77f9b9b46522e8e25d42e4c
Patrick Griffis <tingping@tingping.se>
2016-02-18 08:41:00 -0500

    Consistently include license headers
    Fixes #13

--- src/makeformats.py
+++ src/makeformats.py
@@ -4,14 +4,49 @@
 #
 # Just a short script to generate our FORMAT_REC
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
 
 import sys,os,re
+
+GPL_LICENSE = """
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
+ */
+
+"""
 
 lines = map(lambda x: x.strip(),open(sys.argv[1],"r").readlines())
 
 out_dir = sys.argv[2] if len(sys.argv) > 2 else "."
 hdr = open(os.path.join(out_dir, "otr-formats.h"), "w")
 srcx = open(os.path.join(out_dir, "hexchat-formats.c"), "w")
+
+srcx.write(GPL_LICENSE)
+hdr.write(GPL_LICENSE)
 
 srcx.write('#include "otr.h"\n');
 srcx.write('FORMAT_REC formats[] = {\n\t')
